0cca8f6df64defa74a0b8afcf086184225cf206a
Fernando José <f.jose@avivatec.com.br>
2019-10-01 10:41:11 -0300

    controle de cache

--- src/1-Front/Fiap.Mvc/wwwroot/service-worker.js
+++ src/1-Front/Fiap.Mvc/wwwroot/service-worker.js
@@ -1,19 +1,44 @@
-﻿const cacheName = 'v2';
+﻿const cacheName = 'v24';
 const resourceToPrecache = [
     '/',
     '/favicon.ico',
     '/css/site.css',
     '/lib/bootstrap/dist/css/bootstrap.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css',
     '/img/logo.jpg',
     '/img/topo.jpg',
     '/img/fiap.png'
 ];
 
 self.addEventListener('install', (event) => {
+
+    // Força a instalação do service worker
+    self.skipWaiting();
+
+    // Adiciona os arquivos no cache
     event.waitUntil(
         caches.open(cacheName)
             .then(cache => cache.addAll(resourceToPrecache))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+
+    // Permitir a atualização do novo service worker
+    self.clients.claim();
+
+    // Limpa o cache
+    event.waitUntil(
+        caches.keys()
+            .then(function (cacheKeys) {
+                var deletePromisses = [];
+                for (var i = 0; i < cacheKeys.length; i++) {
+                    if (cacheKeys[i] !== cacheName) {
+                        deletePromisses.push(caches.delete(cacheKeys[i]));
+                    }
+                }
+
+                return Promisse.all(deletePromisses);
+            })
     );
 });
 
